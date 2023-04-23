62117bdf083e4dd84a37da45f3721a4a2c7d5d4b
Bishan Webeshete <webesheteb@gmail.com>
2023-04-23 01:20:14 +0000

    updated add restaurant function, and context

--- client/src/components/AddRestaurant.js
+++ client/src/components/AddRestaurant.js
@@ -1,15 +1,59 @@
+import React, { useState, useContext } from 'react';
+import RestaurantsContext from '../context/RestaurantsContext'
+
 function AddRestaurant () {
+  const { addRestaurants } = useContext(RestaurantsContext)
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [priceRange, setPriceRange] = useState("Price Range");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('/api/restaurants', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: name,
+          location: location,
+          priceRange: priceRange
+        })
+      });
+      if (!response.ok) {
+        throw new Error('Network response not OK', response.status)
+      }
+      console.log(response)
+      const jsonData = await response.json();
+      console.log(jsonData);
+      addRestaurants(jsonData);
+    } catch(error) {
+      console.error(error);
+    }
+  }
+
   return (
       <div className="mb-4 d-flex justify-content-center form-container">
         <form className="row row-col-4 g-4 form-inline">
           <div className="col-3">
-            <input type="text" className="form-control" placeholder="Name" />
+            <input
+            value={name}
+            onChange={(e)=>setName(e.target.value)}
+            type="text"
+            className="form-control"
+            placeholder="Name" />
           </div>
           <div className="col-3">
-            <input type="password" className="form-control" placeholder="Password" />
+            <input
+            value={location}
+            onChange={(e)=>setLocation(e.target.value)}
+            type="text"
+            className="form-control"
+            placeholder="Location" />
           </div>
           <div className="col-3 add-restaurant-select-container">
-            <select className="custom-select my-1 mr-sm-2 add-restaurant-select">
+            <select value={priceRange} onChange={(e)=>setPriceRange(e.target.value)} className="custom-select my-1 mr-sm-2 add-restaurant-select">
               <option disabled>Price Range</option>
               <option value="1">$</option>
               <option value="2">$$</option>
@@ -19,7 +63,7 @@ function AddRestaurant () {
             </select>
           </div>
           <div className="col-3 add-restaurant-button">
-            <button type="submit" className="btn btn-primary mb-3">Submit</button>
+            <button onClick={handleSubmit} type="submit" className="btn btn-primary mb-3">Submit</button>
           </div>
         </form>
       </div>
