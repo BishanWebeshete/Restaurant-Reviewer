7ad3160f99c4c7e83763228f41d563941a130c13
Bishan Webeshete <webesheteb@gmail.com>
2023-04-27 23:15:03 +0000

    updated styling for review cards

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -3,15 +3,15 @@ import StarRating from './StarRating';
 
 export default function Reviews ({reviews}) {
   return (
-    <div className="row row-cols-3 mb-2">
+    <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 mb-2 w-100">
       {reviews?.map((review) => {
         return (
-          <div key={review.id} className="container card text-white bg-primary mb-3 ms-3" style={{ maxWidth: "30%" }}>
-            <div className=" card-header d-flex justify-content-between">
+          <div key={review.id} className="card text-white mb-3 mx-auto p-2 border-0">
+            <div className=" card-header d-flex justify-content-between bg-primary">
               <span>{review.name}</span>
-              <span><StarRating rating={review.rating} /></span>
+              <span className="d-flex flex-nowrap"><StarRating rating={review.rating} /></span>
             </div>
-            <div className="card-body">
+            <div className="card-body bg-primary rounded-bottom">
               <p className="card-text">{review.review}</p>
             </div>
           </div>
