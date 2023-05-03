60c30273616bacb85799dc9d61b1b8b25f401cd7
Bishan Webeshete <webesheteb@gmail.com>
2023-05-03 23:09:12 +0000

    updated css

--- client/src/components/AddReview.js
+++ client/src/components/AddReview.js
@@ -39,13 +39,13 @@ function AddReview() {
         <form action="">
           <div className="row g-3">
             <div className="form-group col">
-              <label htmlFor="name">Name</label>
+              <label htmlFor="name" className="fw-bolder">Name</label>
               <input onChange={(e) => setName(e.target.value)} value={name} id="name" placeholder="name" type="text" className="form-control"></input>
             </div>
             <div className="form-group col">
-              <label htmlFor="rating">Rating</label>
+              <label htmlFor="rating" className="fw-bolder">Rating</label>
               <div>
-                <select onChange={(e) => setRating(e.target.value)} value={rating} id="rating" className="custom-select">
+                <select onChange={(e) => setRating(e.target.value)} value={rating} id="rating" className="form-select">
                   <option disabled>Rating</option>
                   <option value="1">1</option>
                   <option value="2">2</option>
@@ -57,7 +57,7 @@ function AddReview() {
             </div>
           </div>
           <div className="form-group">
-            <label htmlFor="Review">Review</label>
+            <label htmlFor="Review" className="fw-bolder">Review</label>
             <textarea onChange={(e) => setReviewText(e.target.value)} value={reviewText} id="Review" className="form-control"></textarea>
           </div>
           <button type="submit" onClick={handleSubmit} className="btn btn-primary">Submit</button>
