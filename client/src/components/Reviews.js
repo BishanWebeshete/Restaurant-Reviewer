473ac29dd37903454b2b8a748b8c5ba6ef4b7f46
Bishan Webeshete <webesheteb@gmail.com>
2023-05-05 19:17:56 +0000

    updated reviews component to destructure reviews object

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -4,15 +4,15 @@ import StarRating from './StarRating';
 export default function Reviews ({reviews}) {
   return (
     <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 mb-2 w-100">
-      {reviews?.map((review) => {
+      {reviews?.map(({id, name, rating, review}) => {
         return (
-          <div key={review.id} className="card text-white mb-3 mx-auto p-2 border-0">
+          <div key={id} className="card text-white mb-3 mx-auto p-2 border-0">
             <div className=" card-header d-flex justify-content-between bg-primary">
-              <span>{review.name}</span>
-              <span className="d-flex flex-nowrap"><StarRating rating={review.rating} /></span>
+              <span>{name}</span>
+              <span className="d-flex flex-nowrap"><StarRating rating={rating} /></span>
             </div>
             <div className="card-body bg-primary rounded-bottom">
-              <p className="card-text">{review.review}</p>
+              <p className="card-text">{review}</p>
             </div>
           </div>
         )
