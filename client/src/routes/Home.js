b1b8cb37fc3e3926792bd2be646e96222d726065
Bishan Webeshete <webesheteb@gmail.com>
2023-05-19 00:06:57 +0000

    updated navbar

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,10 +1,12 @@
 import Header from '../components/Header';
 import AddStore from '../components/AddStore';
 import StoreList from '../components/StoreList';
+import Navbar from '../components/Navbar';
 
 function Home() {
   return (
     <div>
+      <Navbar />
       <Header />
       <AddStore />
       <StoreList />
