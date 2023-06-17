51cf8d7fc09fd3d487265afcc8fc96af6b91e51b
Bishan Webeshete <webesheteb@gmail.com>
2023-06-17 00:37:31 +0000

    updated navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,7 +7,6 @@ function Navbar() {
   return (
     <nav className="navbar navbar-light bg-secondary d-flex justify-content-between display-5">
       <a className='text-black retail-rater-text' href="/">Retail-Rater</a>
-      {/* <a className="navbar-brand mb-0 h1 text-white navbar-text" href="/">Stores</a> */}
       <button className="sign-out-button" onClick={handleSignOut}>Sign Out</button>
     </nav>
   )
