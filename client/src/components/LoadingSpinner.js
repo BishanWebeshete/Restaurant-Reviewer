b1836e747ae2168573950bf1f1f03e088848ec75
Bishan Webeshete <webesheteb@gmail.com>
2023-06-27 01:00:41 +0000

    updated loading spinner

--- client/src/components/LoadingSpinner.js
+++ client/src/components/LoadingSpinner.js
@@ -1,6 +1,6 @@
 export default function LoadingSpinner () {
   return (
-    <div className="loading-container d-flex justify-content-center">
+    <div className="loading-container d-flex align-items-center justify-content-center">
       <div className="text-center">
         <div className="spinner-border text-light" role="status">
           <span className="visually-hidden">Loading...</span>
